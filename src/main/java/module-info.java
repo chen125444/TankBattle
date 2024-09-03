4b87df142cc3ffa61e5365e6042205816872436f
陳 <144702249+chen125444@users.noreply.github.com>
2024-09-03 15:49:40 +0800

    Initial commit

---
+++ src/main/java/module-info.java
@@ -0,0 +1,12 @@
+module com.jr.tankbattle {
+    requires javafx.controls;
+    requires javafx.fxml;
+
+
+    opens com.jr.tankbattle to javafx.fxml;
+    exports com.jr.tankbattle;
+    exports com.jr.tankbattle.ui;
+    opens com.jr.tankbattle.ui to javafx.fxml;
+    exports com.jr.tankbattle.controller;
+    opens com.jr.tankbattle.controller to javafx.fxml;
+}
