3e4292afa5251bbeba91ff048f157dd9bbfa7d56
0701su <2494450399@qq.com>
2024-09-03 22:06:31 +0800

    枚举类和抽象父类

---
+++ src/main/java/com/jr/tankbattle/controller/AbstractObject.java
@@ -0,0 +1,11 @@
+package com.jr.tankbattle.controller;
+
+import javafx.scene.image.Image;
+
+public  abstract class AbstractObject {
+    //坐标
+    private int x;
+    private int y;
+    //图像
+    private Image image;
+}
---
+++ src/main/java/com/jr/tankbattle/controller/Direction.java
@@ -0,0 +1,8 @@
+package com.jr.tankbattle.controller;
+
+public enum Direction {
+    UP,
+    DOWN,
+    RIGHT,
+    LEFT
+}
