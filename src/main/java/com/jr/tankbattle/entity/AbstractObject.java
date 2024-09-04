ec665e429f8d31651ffe6896b169c624a14d1ba5
0701su <2494450399@qq.com>
2024-09-04 09:15:25 +0800

    1

--- src/main/java/com/jr/tankbattle/entity/AbstractObject.java
+++ src/main/java/com/jr/tankbattle/entity/AbstractObject.java
@@ -1,11 +1,77 @@
 package com.jr.tankbattle.entity;
 
+import com.jr.tankbattle.controller.StartScr;
 import javafx.scene.image.Image;
 
-public  abstract class AbstractObject {
-    //坐标
+public abstract class AbstractObject {
+    // 坐标
     private int x;
     private int y;
-    //图像
+    // 图像
     private Image image;
+    // 界面
+    private StartScr startScr;
+    // 对象是否存活
+    private boolean alive;
+
+    // 构造方法
+    public AbstractObject(int x, int y, Image image, StartScr startScr) {
+        this.x = x;
+        this.y = y;
+        this.image = image;
+        this.startScr = startScr;
+        this.alive = true;
+    }
+
+    // 构造方法重载，允许不设置图像
+    public AbstractObject(int x, int y, StartScr startScr) {
+        this(x, y, null, startScr);
+    }
+
+    // Getter 和 Setter 方法
+    public int getX() {
+        return x;
+    }
+
+    public void setX(int x) {
+        this.x = x;
+    }
+
+    public int getY() {
+        return y;
+    }
+
+    public void setY(int y) {
+        this.y = y;
+    }
+
+    public Image getImage() {
+        return image;
+    }
+
+    public void setImage(Image image) {
+        this.image = image;
+    }
+
+    public StartScr getStartScr() {
+        return startScr;
+    }
+
+    public void setStartScr(StartScr startScr) {
+        this.startScr = startScr;
+    }
+
+    public boolean isAlive() {
+        return alive;
+    }
+
+    public void setAlive(boolean alive) {
+        this.alive = alive;
+    }
+
+    // 抽象方法，由子类实现
+    public abstract void move();  // 移动方法
+    public abstract void draw();  // 绘制对象方法
+    public abstract void checkCollision(AbstractObject other);  // 碰撞检测
 }
+
--- src/main/java/com/jr/tankbattle/entity/Tank.java
+++ src/main/java/com/jr/tankbattle/entity/Tank.java
@@ -1,4 +1,34 @@
 package com.jr.tankbattle.entity;
 
-public class Tank {
+import com.jr.tankbattle.controller.StartScr;
+import javafx.scene.image.Image;
+
+public class Tank extends AbstractObject{
+    private Direction direction = Direction.UP;
+    //坦克速度
+    private int speed;
+    //坦克尺寸
+    private int width;
+    private int height;
+    public Tank(int x, int y, int width, int height, int speed, Image image, StartScr startScr) {
+        super(x, y, image, startScr);
+        this.width = width;
+        this.height = height;
+        this.speed = speed;
+    }
+
+    @Override
+    public void move() {
+        // 实现坦克的移动逻辑
+    }
+
+    @Override
+    public void draw() {
+        // 实现坦克的绘制逻辑
+    }
+
+    @Override
+    public void checkCollision(AbstractObject other) {
+        // 实现坦克与其他对象的碰撞检测逻辑
+    }
 }
